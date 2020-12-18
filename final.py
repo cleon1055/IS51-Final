79272689fd50e2e6681f4f62cad68ac8b30fbce2
cleon1055 <cleon1055@gmail.com>
2020-12-17 18:54:23 -0800

    finished standard-english

---
+++ final.py
@@ -0,0 +1,17 @@
+
+"""                 Structured-English
+
+This program will display the total number of grades, average grade among those grades, 
+and percentage of grades that fall above the average based on a file with grades. 
+The program will first open the grades file called final.txt
+Next, the program will read grades in file and make a list. Once the file is read into memory is will be closed. 
+
+Based of the list, the program will then display the total number of grades in the list depending on how many 
+grades there are, which should be 24.
+Next, we will calculate the avergae grade of the grades in list and the program will display the percentage, which
+should be 83.25%.
+Then we will calculate the percentage of grades from the list above the average grade and the program will display 
+the percentage of grades above the average.  
+
+
+"""
